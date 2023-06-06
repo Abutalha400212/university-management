3967ca9b946bd322b2f101c0e4a4f93ccf155d97
Abu Talha <abutalha141999@gmail.com>
2023-06-06 09:34:28 +0600

    setup eslint prettier

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
-
-app.use(cors());
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+app.use(cors())
 
 //Parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working successfully");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working successfully')
+})
 
-export default app;
+export default app
